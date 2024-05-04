ccf1b756c17c052e7c55f51451906b91fde89b9d
Carlos Guillermo Baldracco <cgbaldracco@gmail.com>
2024-05-03 21:48:15 -0300

    Cambio MotorDeSugerencias a clase abstracta para reutilizar codigo de clases hijas. No añado cambio de utilizacion de biblioteca guava porque... no funciona con listas y no tengo ganas de investigar como lo haria con sets ahora.

--- src/main/java/ar/edu/utn/frba/dds/queMePongo/sugerencias/MotorDeSugerenciasBasico.java
+++ src/main/java/ar/edu/utn/frba/dds/queMePongo/sugerencias/MotorDeSugerenciasBasico.java
@@ -1,35 +1,12 @@
 package ar.edu.utn.frba.dds.queMePongo.sugerencias;
 
-import ar.edu.utn.frba.dds.queMePongo.prenda.Prenda;
 import ar.edu.utn.frba.dds.queMePongo.Usuario;
-import java.util.ArrayList;
 import java.util.List;
 
-public class MotorDeSugerenciasBasico implements MotorDeSugerencias {
+public class MotorDeSugerenciasBasico extends MotorDeSugerencias {
   public List<Sugerencia> generarSugerencias(Usuario usuario) {
-    List<Prenda> prendasSuperiores = usuario.obtenerPrendasSuperiores();
-    List<Prenda> prendasInferiores = usuario.obtenerPrendasInferiores();
-    List<Prenda> prendasCalzado = usuario.obtenerPrendasCalzado();
-
-    List<Sugerencia> sugerencias = generarCombinacionesDePrendas(prendasSuperiores, prendasInferiores, prendasCalzado);
-    return sugerencias;
-  }
-
-  public List<Sugerencia> generarCombinacionesDePrendas(List<Prenda> superiores, List<Prenda> inferiores, List<Prenda> calzados) {
-    List<Sugerencia> sugerencias = new ArrayList<>();
-    Prenda superior;
-    Prenda inferior;
-    Prenda calzado;
-    for (int i = 0; i < superiores.toArray().length; i++) {
-      superior = superiores.get(i);
-      for (int j = 0; j < inferiores.toArray().length; j++) {
-        inferior = inferiores.get(j);
-        for (int k = 0; k < calzados.toArray().length; k++) {
-          calzado = calzados.get(k);
-          sugerencias.add(new Sugerencia(superior, inferior, calzado));
-        }
-      }
-    }
+    separarPorCategoria(usuario);
+    generarCombinacionesDePrendas();
     return sugerencias;
   }
 }
--- src/main/java/ar/edu/utn/frba/dds/queMePongo/sugerencias/MotorDeSugerenciasFormal.java
+++ src/main/java/ar/edu/utn/frba/dds/queMePongo/sugerencias/MotorDeSugerenciasFormal.java
@@ -3,43 +3,24 @@ package ar.edu.utn.frba.dds.queMePongo.sugerencias;
 import ar.edu.utn.frba.dds.queMePongo.prenda.Formalidad;
 import ar.edu.utn.frba.dds.queMePongo.prenda.Prenda;
 import ar.edu.utn.frba.dds.queMePongo.Usuario;
-import java.util.ArrayList;
 import java.util.List;
 
-public class MotorDeSugerenciasFormal implements MotorDeSugerencias {
+public class MotorDeSugerenciasFormal extends MotorDeSugerencias {
   public List<Sugerencia> generarSugerencias(Usuario usuario) {
-    List<Prenda> prendasSuperiores = usuario.obtenerPrendasSuperiores();
-    List<Prenda> prendasInferiores = usuario.obtenerPrendasInferiores();
-    List<Prenda> prendasCalzado = usuario.obtenerPrendasCalzado();
+    separarPorCategoria(usuario);
+    obtenerPrendasValidasParaMotor(usuario);
+    generarCombinacionesDePrendas();
+    return sugerencias;
+  }
 
+  private void obtenerPrendasValidasParaMotor(Usuario usuario) {
     prendasSuperiores = obtenerPrendasValidas(usuario.obtenerEdad(), prendasSuperiores);
     prendasInferiores = obtenerPrendasValidas(usuario.obtenerEdad(), prendasInferiores);
     prendasCalzado = obtenerPrendasValidas(usuario.obtenerEdad(), prendasCalzado);
-
-    List<Sugerencia> sugerencias = generarCombinacionesDePrendas(prendasSuperiores, prendasInferiores, prendasCalzado);
-    return sugerencias;
   }
 
   List<Prenda> obtenerPrendasValidas(Integer edad, List<Prenda> prendas){
     return edad > 55 ? prendas.stream().filter(prenda ->
         !(prenda.obtenerFormalidad() == Formalidad.INFORMAL)).toList() : prendas;
-  }
-
-  public List<Sugerencia> generarCombinacionesDePrendas(List<Prenda> superiores, List<Prenda> inferiores, List<Prenda> calzados) {
-    List<Sugerencia> sugerencias = new ArrayList<>();
-    Prenda superior;
-    Prenda inferior;
-    Prenda calzado;
-    for (int i = 0; i < superiores.toArray().length; i++) {
-      superior = superiores.get(i);
-      for (int j = 0; j < inferiores.toArray().length; j++) {
-        inferior = inferiores.get(j);
-        for (int k = 0; k < calzados.toArray().length; k++) {
-          calzado = calzados.get(k);
-          sugerencias.add(new Sugerencia(superior, inferior, calzado));
-        }
-      }
-    }
-    return sugerencias;
   }
 }
